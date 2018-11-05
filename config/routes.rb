a865d97844f1779caf5473863709262cb11fc85d
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-05 18:48:29 -0500

    implemented return to home button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'index', to: 'flats#index', as: :index
-  get 'show/:id', to: 'flats#show', as: :flat
+  get 'show/:id', to: 'flats#show', as: :flat #:id makes the id dynamic, so we can insert any value
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
