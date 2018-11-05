e7ce7122c8ed5ddbf226a69dcb6b5b13ca27e480
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-05 18:44:15 -0500

    implemented live code

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,17 @@
+require "open-uri"
+
+class FlatsController < ApplicationController
+  def index
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
+  end
+
+  def show
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
+
+    @showFlat = @flats.find do |flat|
+      flat["id"].to_i == params["id"].to_i
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'index', to: 'flats#index', as: :index
+  get 'show/:id', to: 'flats#show', as: :flat
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
